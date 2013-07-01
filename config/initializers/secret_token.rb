b3505524a17d8247cd636290a91342e8bbe8b6c2
Alex Levine <a17levine@gmail.com>
2013-07-01 10:03:48 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlexBlog::Application.config.secret_token = '3d249a01115cb067345d53693e2d276a9d3783cc1b8a921f1ccc57446d5b9f2fd3fad6c5b3bcb430d5b073c1fabf47e5dfbfd6c895e1ffae70c276047b336144'
